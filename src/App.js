69b586cf7c14d58d2efbcd868b99ecb52878ad9c
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-04-26 22:48:05 +0200

    adding attribution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import PreviewCard from './components/PreviewCard';
+import Appendix from './components/Appendix';
 
 const App = () => {
   return (
     <div className="wrapper">
       <PreviewCard />
+      <Appendix />
     </div>
   );
 };
